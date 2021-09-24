9f9a8c771920535f2bb9d0ef4e87c08e495a8ab4
Cute <cute@sweet.pink>
2021-09-24 19:44:46 +0200

    Prepared Debug

---
+++ ui/debug_screen/DebugScreen.gd
@@ -0,0 +1,6 @@
+extends Control
+
+onready var label_fps = $VBoxContainer/FPSLabel
+onready var label_ene_number_label = $VBoxContainer/EneNumberLabel
+onready var button_add_enemies = $VBoxContainer/AddEnemies
+onready var button_decrease_enemies = $VBoxContainer/DecreaseEnemies
