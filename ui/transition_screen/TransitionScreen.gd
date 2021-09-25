e3f063a25a58260a1fd0732df336464f537df893
Cute <cute@sweet.pink>
2021-09-25 20:22:13 +0200

    Able to skip transition screen and menu egg

--- ui/transition_screen/TransitionScreen.gd
+++ ui/transition_screen/TransitionScreen.gd
@@ -1,12 +1,18 @@
-extends Node2D
+extends Control
 
 onready var animated_sprite = $AnimatedSprite
+
 
 func _ready():
 	animated_sprite.connect("animation_finished", self, "change_screen")
 	
 	animated_sprite.frame = 0
 	animated_sprite.play("start")
+
+
+func _input(event):
+	if event is InputEventMouseButton or event is InputEventScreenTouch:
+		change_screen()
 
 
 func change_screen():
