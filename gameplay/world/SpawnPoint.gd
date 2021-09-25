0ab52e338d5d77cd6e5c94b31b1ccedd978eb0b5
Cute <cute@sweet.pink>
2021-09-25 09:33:00 +0200

    Proper spawning point

--- gameplay/world/SpawnPoint.gd
+++ gameplay/world/SpawnPoint.gd
@@ -8,17 +8,17 @@ func get_rand_point(player_pos: Vector2) -> Vector2:
 	
 	var side = randi()%4
 	if side == LEFT: 
-		result.y = (randi()% int(Global.wind_size.y)) + (player_pos.y - Global.wind_size.y)
+		result.y = (randi()% int(Global.wind_size.y*2)) + (player_pos.y - Global.wind_size.y)
 		result.x = player_pos.x - Global.wind_size.x
 	if side == RIGHT: 
-		result.y = (randi()% int(Global.wind_size.y)) + (player_pos.y - Global.wind_size.y)
+		result.y = (randi()% int(Global.wind_size.y*2)) + (player_pos.y - Global.wind_size.y)
 		result.x = player_pos.x + Global.wind_size.x
 	if side == TOP:
 		result.y = player_pos.y - Global.wind_size.y
-		result.x = (randi()% int(Global.wind_size.x)) + (player_pos.x - Global.wind_size.x)
+		result.x = (randi()% int(Global.wind_size.x*2)) + (player_pos.x - Global.wind_size.x)
 	if side == DOWN:
 		result.y = player_pos.y + Global.wind_size.y
-		result.x = (randi()% int(Global.wind_size.x)) + (player_pos.x - Global.wind_size.x)
+		result.x = (randi()% int(Global.wind_size.x*2)) + (player_pos.x - Global.wind_size.x)
 	
 	return result
 
@@ -29,10 +29,10 @@ func get_rand_point_top_down(player_pos: Vector2) -> Vector2:
 	var side = randi()%2
 	if side == TOP:
 		result.y = player_pos.y - Global.wind_size.y
-		result.x = (randi()% int(Global.wind_size.x)) + (player_pos.x - Global.wind_size.x)
+		result.x = (randi()% int(Global.wind_size.x*2)) + (player_pos.x - Global.wind_size.x)
 	if side == DOWN:
 		result.y = player_pos.y + Global.wind_size.y
-		result.x = (randi()% int(Global.wind_size.x)) + (player_pos.x - Global.wind_size.x)
+		result.x = (randi()% int(Global.wind_size.x*2)) + (player_pos.x - Global.wind_size.x)
 	
 	return result
 
