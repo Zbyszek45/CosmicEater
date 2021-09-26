d72ffa992ddb68b921a8d4a6fdc9b2be08b8f923
Cute <cute@sweet.pink>
2021-09-26 21:55:50 +0200

    popupbase created

---
+++ ui/popups/popup_base/PopupBase.gd
@@ -0,0 +1,29 @@
+extends PopupPanel
+
+
+onready var accept_button = $MargCont/Vbox/Buttons/AcceptButton
+onready var icon = $MargCont/Vbox/Icon
+onready var title = $MargCont/Vbox/Title
+onready var content = $MargCont/Vbox/Content
+
+signal finished
+var on_accept_method = "_on_accept"
+
+func _ready():
+	accept_button.connect("pressed", self, "accept")
+	get_tree().paused = true
+
+
+func accept():
+	if has_method(on_accept_method):
+		call(on_accept_method)
+	else:
+		Global.show_error("res://ui/popups/popup_base/PopupBase.gd" \
+		, "Inherited scene dont have method '_on_accept_and_resume'")
+	
+	unpause_tree()
+	emit_signal("finished")
+
+
+func unpause_tree():
+	get_tree().paused = false
