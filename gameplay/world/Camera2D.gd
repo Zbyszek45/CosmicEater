a39edaab6fb145d75e44d4e1876f505b77635bf6
Cute <cute@sweet.pink>
2021-09-26 23:57:31 +0200

    Particles for world

--- gameplay/world/Camera2D.gd
+++ gameplay/world/Camera2D.gd
@@ -1,10 +1,25 @@
 extends Camera2D
 
 onready var tween = $Tween
+onready var dust_particles = $DustParticles
+onready var star_particles = $StarParticles
 
 func _ready():
 	GameEvents.connect("player_grew_up", self, "animate_camera")
-
+	
+	if dust_particles.emission_shape == CPUParticles2D.EMISSION_SHAPE_RECTANGLE:
+		dust_particles.emission_rect_extents.x = Global.wind_size.x + 100
+		dust_particles.emission_rect_extents.y = Global.wind_size.y + 100
+	else:
+		Global.show_error("res://gameplay/world/Camera2D.gd"\
+		, "Particles dont have rect emmision shape")
+	
+	if star_particles.emission_shape == CPUParticles2D.EMISSION_SHAPE_RECTANGLE:
+		star_particles.emission_rect_extents.x = Global.wind_size.x + 100
+		star_particles.emission_rect_extents.y = Global.wind_size.y + 100
+	else:
+		Global.show_error("res://gameplay/world/Camera2D.gd"\
+		, "Particles dont have rect emmision shape")
 
 func animate_camera(size, amount):
 	tween.interpolate_property(self, "zoom", Vector2(1-amount, 1-amount), Vector2(1, 1) \
